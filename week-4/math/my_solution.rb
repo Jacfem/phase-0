14fa7a8ed3d0df40b4efa41d32491622c21e3648
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-02-08 19:37:53 -0500

    Post Math and Variable Exercises

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,23 @@
+# 4.3.2 Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ num_1 * num_2
+end
+
+#We can force numbers to be in float mode with .to_f
+def divide(num_1, num_2)
+ num_1.to_f / num_2.to_f
+end
--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -3,11 +3,31 @@
 #Finally, it should greet the person using their full name.
 
 
-puts "Hi! What\'s your first name?"
+puts "Hi! What is your first name?"
   first_name = gets.chomp
-puts "Awesome! What\'s your middle name?"
+puts "What is your middle name?"
   middle_name = gets.chomp
-puts "And I bet you have a last name! Which is?"
+puts "What is your last name?"
   last_name = gets.chomp
-  full_name = first_name + ' ' + middle_name + ' ' + last_name
-puts "Great to meet you, '+ full_name + '!"
+
+full_name = first_name + " " + middle_name + " " + last_name
+puts "Great to meet you, " + full_name + "!"
+
+
+
+#Bigger, better favorite number:
+#Write a program that asks for a person's favorite number.
+#Have your program add 1 to the number,
+#and then suggest the result as a bigger and better number.
+
+#Pseudocode: Ask for a number
+#Record number as integer in order to do addition
+#Add one to that number
+#Record new number
+#Convert new number to string
+#Return suggestion of new number as a better number
+
+puts "What is your favorite number?"
+  favorite_num = gets.chomp.to_i
+  better_num = favorite_num + 1
+puts "That's cool and all, but don't you think " + better_num.to_s + " is a bigger, better number?"

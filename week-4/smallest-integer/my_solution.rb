25a40cc5d7427101c17c86ccc0b4e1f3914b8b06
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-02-09 20:48:50 -0500

    Push Challenge Solutions

---
+++ week-4/smallest-integer/my_solution.rb
@@ -0,0 +1,26 @@
+# Smallest Integer
+
+# I worked on this challenge [by myself, with: ].
+
+# smallest_integer is a method that takes an array of integers as its input
+# and returns the smallest integer in the array
+#
+# +list_of_nums+ is an array of integers
+# smallest_integer(list_of_nums) should return the smallest integer in +list_of_nums+
+#
+# If +list_of_nums+ is empty the method should return nil
+
+# Your Solution Below
+def smallest_integer(list_of_nums)
+
+end
+smallest_integer(1, 4, 6)
+
+=begin
+Pseudocode:
+Get integers as input
+If there is an array of integers, sort them low to high
+Return lowest integer
+If it's an empty list, return nil
+
+=end
